92c859fca383f0adb2459f2df4c9f945d0406b78
Valentin Trifonov <valentintrifonov03@gmail.com>
2018-03-10 02:04:33 +0200

    added problem one solution

---
+++ intro-and-basic-syntax/exercise/problem-one.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = int.Parse(Console.ReadLine());
+            var secondLine = int.Parse(Console.ReadLine());
+            var thirdLine = int.Parse(Console.ReadLine());
+            var fourthLine = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2} {3}", firstLine.ToString("D4"),
+                secondLine.ToString("D4"),
+                thirdLine.ToString("D4"),
+                fourthLine.ToString("D4"));
+        }
+    }
+}
