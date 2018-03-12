e138c69731c1b6b3788b5969f901be71aa4ee79e
otkata <36444943+otkata@users.noreply.github.com>
2018-03-12 17:05:40 +0200

    Problem 3/MilesToKm.cs Problem 5/CharacterStats.cs

---
+++ intro-and-basic-syntax/exercise/Problem 3/MilesToKm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Miles_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double m = double.Parse(Console.ReadLine());
+            double km = m * 1.60934;
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
---
+++ intro-and-basic-syntax/exercise/Problem 5/CharacterStats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int health = int.Parse(Console.ReadLine());
+            int healthMax = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int energyMax = int.Parse(Console.ReadLine());
+
+            string rpt1 = new String('|', health);
+            string rpt2 = new String('.', healthMax - health);
+            string rpt3 = new String('|', energy);
+            string rpt4 = new String('.', energyMax - energy);
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: |{rpt1}{rpt2}|");
+            Console.WriteLine($"Energy: |{rpt3}{rpt4}|");
+            
+        }
+
+    }
+}
