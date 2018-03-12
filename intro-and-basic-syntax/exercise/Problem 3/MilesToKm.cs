30e49292fcb68b973508fbbefc194074681e7df8
Tsvetana <tsvetish@abv.bg>
2018-03-13 00:12:46 +0200

    documenting the code of Solution of Problem 3;

--- intro-and-basic-syntax/exercise/Problem 3/MilesToKm.cs
+++ intro-and-basic-syntax/exercise/Problem 3/MilesToKm.cs
@@ -6,6 +6,7 @@ namespace Miles_to_Kilometers
     {
         static void Main(string[] args)
         {
+            // We receive from the console value in miles -> double;
             double m = double.Parse(Console.ReadLine());
             // We calculate km
             double km = m * 1.60934;
