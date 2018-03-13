b9cb3d34c133296806d261db61e4fe0ca82fe9d3
otkata <36444943+otkata@users.noreply.github.com>
2018-03-13 10:11:56 +0200

    documents the code of Solution of Problem 1

--- intro-and-basic-syntax/exercise/Problem 1/Debit-Card-Number.cs
+++ intro-and-basic-syntax/exercise/Problem 1/Debit-Card-Number.cs
@@ -10,11 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // We receive from the Console 4 integers 
             var firstLine = int.Parse(Console.ReadLine());
             var secondLine = int.Parse(Console.ReadLine());
             var thirdLine = int.Parse(Console.ReadLine());
             var fourthLine = int.Parse(Console.ReadLine());
 
+            // Print them in 4-digit debit card format 
             Console.WriteLine("{0} {1} {2} {3}", firstLine.ToString("D4"),
                 secondLine.ToString("D4"),
                 thirdLine.ToString("D4"),
