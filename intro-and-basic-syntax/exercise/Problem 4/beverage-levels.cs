d7e0a9010ae234ee1b1be64a7e60656f93aad26b
otkata <36444943+otkata@users.noreply.github.com>
2018-03-13 10:04:51 +0200

    documents the code of Solution of Problem 4

--- intro-and-basic-syntax/exercise/Problem 4/beverage-levels.cs
+++ intro-and-basic-syntax/exercise/Problem 4/beverage-levels.cs
@@ -10,14 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // We receive from the Console the product name
             var product = Console.ReadLine();
+
+            //In the next 3 lines we receive volume, energy and sugar content of our products
             var volume = int.Parse(Console.ReadLine());
             var energyPer100ml = int.Parse(Console.ReadLine());
             var sugarPer100ml = int.Parse(Console.ReadLine());
 
+            //We calculate....
             double totalEnergy = energyPer100ml / 100.0 * volume;
             double totalSugar = sugarPer100ml * volume / 100.0;
 
+            //TODO
             Console.WriteLine("{0}ml {1}:", volume, product);
             Console.WriteLine("{0}kcal, {1}g sugars", totalEnergy, totalSugar);
         }
