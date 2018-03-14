c6f660320674ac47ddd29df0d8c11db284628610
vallecbg <valioplayofficial@gmail.com>
2018-03-14 21:11:22 +0200

    Changing the variable names of Problem 2.

--- intro-and-basic-syntax/exercise/Problem 2/Rectangle Area.cs
+++ intro-and-basic-syntax/exercise/Problem 2/Rectangle Area.cs
@@ -11,9 +11,9 @@ namespace rectangle_area
             // We need to get some items:
 
             // We receive from the Console the rectangle's width -> a double;
-            double width = double.Parse(Console.ReadLine());
+            double rectangleWidth = double.Parse(Console.ReadLine());
             // We height from the Console the rectangle's width -> a double;
-            double height = double.Parse(Console.ReadLine());
+            double rectangleHeight = double.Parse(Console.ReadLine());
             /* We need to calculate the rectangle's area
              Sample - We receive:
              Width: 2
@@ -21,9 +21,9 @@ namespace rectangle_area
              Area: Width * Height = 2 * 7 = 14
               */
             // Also, we need to make the area to double because we are multiplying floating point numbers
-            double area = width * height;
+            double rectangleArea = rectangleWidth * rectangleHeight;
             // In the end, we print the area on the Console, rounded to the 2nd character after the decimal point.
-            Console.WriteLine("{0:f2}", area);
+            Console.WriteLine("{0:f2}", rectangleArea);
         }
     }
 }
