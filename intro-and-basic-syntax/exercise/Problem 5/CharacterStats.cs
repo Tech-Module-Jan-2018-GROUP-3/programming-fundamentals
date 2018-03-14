9d8bdefde0c822795362c89071532a392ac4f909
vallecbg <valioplayofficial@gmail.com>
2018-03-14 21:14:59 +0200

    Changing the variable names of Problem 5.

--- intro-and-basic-syntax/exercise/Problem 5/CharacterStats.cs
+++ intro-and-basic-syntax/exercise/Problem 5/CharacterStats.cs
@@ -9,26 +9,26 @@ namespace CharacterStats
             // So we need to make character stats for user
 
             // We receive name from the Console -> a string;
-            string name = Console.ReadLine();
+            string characterName = Console.ReadLine();
             /* We also receive 
              Current Health -> an integer
              Maximum Health -> an integer
              Current Energy -> an integer
              Maximum Energy -> an integer
              */
-            int health = int.Parse(Console.ReadLine());
-            int healthMax = int.Parse(Console.ReadLine());
-            int energy = int.Parse(Console.ReadLine());
-            int energyMax = int.Parse(Console.ReadLine());
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
 
             // Before we print them we'll make the values of health and energy in a string so we'll print the output easily
-            string currentHealthString = new String('|', health);
-            string maximumHealthString = new String('.', healthMax - health);
-            string currentEnergyString = new String('|', energy);
-            string maximumEnergyString = new String('.', energyMax - energy);
+            string currentHealthString = new String('|', currentHealth);
+            string maximumHealthString = new String('.', maxHealth - currentHealth);
+            string currentEnergyString = new String('|', currentEnergy);
+            string maximumEnergyString = new String('.', maxEnergy - currentEnergy);
 
             // Printing the name
-            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Name: {characterName}");
             // Printing the health bar
             Console.WriteLine($"Health: |{currentHealthString}{maximumHealthString}|");
             // Printing the energy bar
